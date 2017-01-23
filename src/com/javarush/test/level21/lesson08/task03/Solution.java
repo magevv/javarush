04b68279529491a080df691638dd28b331c77185
Yuriy Popsuenko <ypo@encode.dk>
2017-01-23 22:43:23 +0200

    level21.lesson08.task02  Cloning

---
+++ src/com/javarush/test/level21/lesson08/task03/Solution.java
@@ -0,0 +1,76 @@
+package com.javarush.test.level21.lesson08.task03;
+
+/* Запретить клонирование
+Разрешите клонировать класс А
+Запретите клонировать класс B
+Разрешите клонировать класс C
+Метод main не участвует в тестировании.
+*/
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+
+        public A clone() throws CloneNotSupportedException {
+            return (A)super.clone();
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public B clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+    }
+
+    public static class C extends B implements Cloneable {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        public C clone() throws CloneNotSupportedException {
+            //C c = (C)super.clone();
+            return new C(getI(), getJ(), getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        A a = new A(1,2);
+        B b = new B(3,4, "B");
+        C c = new C(5,6, "C");
+
+        try {
+            a.clone();
+            b.clone();
+            C clone = null;
+            clone = c.clone();
+            System.out.println(c);
+            System.out.println(clone);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+}
